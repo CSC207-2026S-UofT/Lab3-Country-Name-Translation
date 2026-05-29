b2f6c72753f0e1664cb1bf68ee89fed43bf3b636
kimso34 <skylar.kim@mail.utoronto.ca>
2026-05-29 06:04:38 -0400

    Remove extra subfolder and move src to root

--- src/main/java/org/translation/InLabByHandTranslator.java
+++ src/main/java/org/translation/InLabByHandTranslator.java
@@ -1,9 +1,7 @@
 package org.translation;
 
 import java.util.ArrayList;
-import java.util.HashMap;
 import java.util.List;
-import java.util.Map;
 
 // TODO Task: modify this class so that it also supports the Spanish language code "es" and
 //            one more language code of your choice. Each member of your group should add
@@ -16,9 +14,6 @@ import java.util.Map;
  * the country code "can" to several languages.
  */
 public class InLabByHandTranslator implements Translator {
-    public static final String CANADA = "can";
-    public static final String SPAIN = "spa";
-
     /**
      * Returns the language abbreviations for all languages whose translations are
      * available for the given country.
@@ -28,24 +23,25 @@ public class InLabByHandTranslator implements Translator {
      */
     @Override
     public List<String> getCountryLanguages(String country) {
-        if (CANADA.equals(country)) {
+        // TODO Checkstyle: The String "can" appears 4 times in the file.
+        if ("can".equals(country)) {
             return new ArrayList<>(List.of("de", "en", "zh"));
-        }
-        if (SPAIN.equals(country)) {
-            return new ArrayList<>(List.of("en", "zh"));
         }
         return new ArrayList<>();
     }
+
+    // TODO Checkstyle: Static variable definition in wrong order.
+    public static final String CANADA = "can";
+
     /**
      * Returns the country abbreviations for all countries whose translations are
      * available from this Translator.
      *
      * @return list of country abbreviations for which we have translations available
      */
-
     @Override
     public List<String> getCountries() {
-        return new ArrayList<>(List.of(CANADA, SPAIN));
+        return new ArrayList<>(List.of("can"));
     }
 
     /**
@@ -55,26 +51,24 @@ public class InLabByHandTranslator implements Translator {
      * @param language the language
      * @return the name of the country in the given language or null if no translation is available
      */
-
     @Override
     public String translate(String country, String language) {
-        if (!getCountries().contains(country)){return null;}
-
-        Map<String, String> canMap = new HashMap<>();
-        canMap.put("de","Kanada");
-        canMap.put("en","Canada");
-        canMap.put("zh","加拿大");
-
-        Map<String, String> spaMap = new HashMap<>();
-        spaMap.put("de","Spain");
-        spaMap.put("en","Spain");
-        spaMap.put("zh","西班牙");
-
-        Map<String, Map<String, String>> lanMap = new HashMap<>();
-        lanMap.put(CANADA, canMap);
-        lanMap.put(SPAIN, spaMap);
-
-        return lanMap.get(country).get(language);
+        // TODO Checkstyle: Return count is 5 (max allowed for non-void methods/ lambdas is 2).
+        // TODO Checkstyle: String literal expressions should be on the left side of an equals comparison
+        if (!country.equals("can")) {
+            return null;
+        }
+        if (language.equals("de")) {
+            return "Kanada";
+        }
+        else if (language.equals("en")) {
+            return "Canada";
+        }
+        else if ("zh".equals(language)) {
+            return "加拿大";
+        }
+        else {
+            return null;
         }
     }
 }
